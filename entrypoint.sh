b36046859a0c36bbae6903cbca21ab2b2ea69e89
Grkila <dusangrkovic2002@gmail.com>
2025-07-01 12:18:15 +0200

    Refactor entrypoint.sh to include workspace update and dependency installation steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,15 @@ if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
   source "/opt/ros/${ROS_DISTRO}/setup.bash"
 fi
 
+echo "--- Updating workspace from git repository (git pull) ---"
+git pull
+
+echo "--- Installing dependencies for the workspace ---"
+# The -r flag tells rosdep to continue even if it can't resolve a dependency
+rosdep install --from-paths src -y -i -r
+
+echo "--- Building the workspace ---"
+colcon build --symlink-install --packages-up-to husarion_ugv --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
 # Source workspace environment
 if [ -f "/husarion_ws/install/setup.bash" ]; then
   echo "Sourcing workspace environment: /ros2_ws/install/setup.bash"
